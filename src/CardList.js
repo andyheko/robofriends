cc692f9ff0eeb6c465d0754579c656286a9d21de
andyheko <34779522+andyheko@users.noreply.github.com>
2019-09-27 21:57:05 +0800

    add search box

--- src/CardList.js
+++ src/CardList.js
@@ -2,19 +2,20 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    const cardsArray = robots.map((robot) => {
-        return (
-            <Card
-            key={robot.id}
-            id={robot.id}
-            name={robot.name}
-            email={robot.email}
-            />
-        );
-    })
     return (
         <div>
-            {cardsArray}
+            {
+                robots.map((robot) => {
+                    return (
+                        <Card
+                            key={robot.id}
+                            id={robot.id}
+                            name={robot.name}
+                            email={robot.email}
+                            />
+                    );
+                })
+            }
         </div>
     );
 }
