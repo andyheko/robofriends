1b8b64715e92a96bf61502c7aff7054d8661c005
andyheko <34779522+andyheko@users.noreply.github.com>
2019-10-02 23:49:24 +0800

    add scroll-down function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import SearchBox from './SearchBox'
-import {robots} from './robots'
+import SearchBox from './SearchBox';
+import Scroll from './Scroll';
+import './App.css';
 
 
 class App extends Component {
@@ -37,9 +38,11 @@ class App extends Component {
         } else {
             return(
                 <div className='tc'>
-                    <h1>RoboFriends</h1>
+                    <h1 className='f1'>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
-                    <CardList robots = {filteredRobots}/>
+                    <Scroll>
+                        <CardList robots = {filteredRobots}/>
+                    </Scroll>
                 </div>
             );
         }
